e94b35cbe410ef4c2789d42b23e54a12e0c96937
shauryagupta45 <shauryagupta4501@gmail.com>
2023-12-16 23:32:07 +0530

    update

--- lib/users.dart
+++ lib/users.dart
@@ -1,13 +1,69 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+@immutable
 class User {
   final String name;
   final int age;
-  User({required this.name, required this.age});
+  const User({
+    required this.name,
+    required this.age,
+  });
+
+  User copyWith({
+    String? name,
+    int? age,
+  }) {
+    return User(
+      name: name ?? this.name,
+      age: age ?? this.age,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'name': name,
+      'age': age,
+    };
+  }
+
+  factory User.fromMap(Map<String, dynamic> map) {
+    return User(
+      name: map['name'] as String,
+      age: map['age'] as int,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory User.fromJson(String source) => User.fromMap(json.decode(source) as Map<String, dynamic>);
+
+  @override
+  String toString() => 'User(name: $name, age: $age)';
+
+  @override
+  bool operator ==(covariant User other) {
+    if (identical(this, other)) return true;
+  
+    return 
+      other.name == name &&
+      other.age == age;
+  }
+
+  @override
+  int get hashCode => name.hashCode ^ age.hashCode;
 }
 
 //This will help to change the User info
 class UserNotifier extends StateNotifier<User> {
   UserNotifier(
-      super.state); // We need to call constructor otherwise it'll give an error
+      super.state); // We need to call constructor otherwise it'll give an error. The 'state' is of same data type as in the function definition's angular bracket i.e. here : User
+
+  void updateName(String n) {
+    // state.name = n ; We cant write this, coz this class is immutable
+    state = User(name: n, age: state.age);
+  }
 }
